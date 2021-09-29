3023df01507f5d2291b3915bc19b1d24f01018d9
GornHub123 <89460673+GornHub123@users.noreply.github.com>
2021-09-29 15:46:32 +0700

    Add files via upload

---
+++ bt5.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.Scanner;
+public class bt5 {
+
+    
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input floating-point number: ");
+        double x = in.nextDouble();
+        System.out.print("Input floating-point another number: ");
+        double y = in.nextDouble();
+
+        x = Math.round(x * 1000);
+        x = x / 1000;
+
+        y = Math.round(y * 1000);
+        y = y / 1000;
+
+        if (x == y)
+        {
+            System.out.println("They are the same up to three decimal places");
+        }
+        else
+        {
+            System.out.println("They are different");
+        }
+    }
+}
